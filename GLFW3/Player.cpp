b8417c70a3c52d07ee165eccefa3ade458ae9500
William Meaton <will.meaton@gmail.com>
2016-03-09 17:06:50 +0000

    Cleaned up and optimised the player mining

--- GLFW3/Player.cpp
+++ GLFW3/Player.cpp
@@ -14,8 +14,9 @@ Player::Player(const Math::Vector2D &scene, const Math::Vector2D &position, cons
 }
 
 void Player::update(){
+    int thisMouseX = inputHandler->getMouseX();
+    int thisMouseY = inputHandler->getMouseY();
     bool movement = false;
-//    this->gravity();
     Math::Vector2D sv;
     //on player movement
     if(inputHandler->getDOWN()){
@@ -34,7 +35,6 @@ void Player::update(){
         sv.x += -speed;
         movement = true;
     }
-//    std::cout << sv.x << " " << sv.y << std::endl;
     worldHandler->movementCheck(editPosition(), sv, editScene(), true, true);
     
 //    if(inputHandler->getSPACE() && getVelocity().y == 0){
@@ -52,30 +52,26 @@ void Player::update(){
     if(!movement){
         //if the LMB is pressed and you're not already mining, start the checks to start mining
         if(inputHandler->getMOUSE0() && !mining){
-            
             //get the distance between the mouse and the player
-            float distance = Math::vectorDistance(getPosition().x+SPRITE_SIZE/2, getPosition().y+SPRITE_SIZE/2, inputHandler->getMouseX(), inputHandler->getMouseY());
+            float distance = Math::vectorDistance(getPosition().x+SPRITE_SIZE/2, getPosition().y+SPRITE_SIZE/2, thisMouseX, thisMouseY);
             
             //only if the distance is less than the spritesize*3 then we should start mining
             if(distance < SPRITE_SIZE*3){
-                miningTime = worldHandler->getTile(inputHandler->getMouseX(), inputHandler->getMouseY())->strength;
-                miningType = worldHandler->getTile(inputHandler->getMouseX(), inputHandler->getMouseY())->textureCode;
-                miningX = inputHandler->getMouseX()/SPRITE_SIZE;
-                miningY = inputHandler->getMouseY()/SPRITE_SIZE;
-                miningX*=SPRITE_SIZE;
-                miningY*=SPRITE_SIZE;
-
-                mining = true;
-                timer.start();
+                WorldHandler::Tile tile = *worldHandler->getTile(thisMouseX, thisMouseY);
+                if(tile.solid){
+                    miningTime = tile.strength;
+                    miningType = tile.textureCode;
+                    miningX = thisMouseX-(thisMouseX%SPRITE_SIZE);
+                    miningY = thisMouseY-(thisMouseY%SPRITE_SIZE);
+                    mining = true;
+                    timer.start();
+                }
             }
         }
         //if you're currently mining then check you're still holding that LMB
         if(mining){
-            
-            int x = inputHandler->getMouseX()/SPRITE_SIZE;
-            int y = inputHandler->getMouseY()/SPRITE_SIZE;
-            x*=SPRITE_SIZE;
-            y*=SPRITE_SIZE;
+            int x = thisMouseX-(thisMouseX%SPRITE_SIZE);
+            int y = thisMouseY-(thisMouseY%SPRITE_SIZE);
             
             //check if you're still on the original tile
             if(x != miningX || y!= miningY){
@@ -85,7 +81,7 @@ void Player::update(){
             }else{
                 //if the mining is still happening
                 if(timer.elapsedTime() >= miningTime){
-                    worldHandler->getTile(inputHandler->getMouseX(), inputHandler->getMouseY()) = &worldHandler->getTiles()[0];
+                    worldHandler->getTile(thisMouseX, thisMouseY) = &worldHandler->getTiles()[0];
                     this->stopMining();
                 }
                 else if(timer.elapsedTime() >= miningTime-(miningTime/4)){
@@ -111,7 +107,7 @@ void Player::stopMining(){
 }
 
 void Player::render(){
-    SpriteHandler::getInstance()->get(SPRITE_CODE::player)->draw(this->getPosition());
+    //if mining draw the level of destruction over the current tile
     if(mining){
         switch(miningLevel){
         case 1:
@@ -120,12 +116,10 @@ void Player::render(){
         case 2:
             SpriteHandler::getInstance()->get(destruction2)->draw(miningX, miningY, SPRITE_SIZE, SPRITE_SIZE);
             break;
-            case 3:
-                SpriteHandler::getInstance()->get(destruction3)->draw(miningX, miningY, SPRITE_SIZE, SPRITE_SIZE);
-
-                break;
-                
-            
+        case 3:
+            SpriteHandler::getInstance()->get(destruction3)->draw(miningX, miningY, SPRITE_SIZE, SPRITE_SIZE);
+            break;
         }
     }
+    SpriteHandler::getInstance()->get(SPRITE_CODE::player)->draw(this->getPosition());
 }
